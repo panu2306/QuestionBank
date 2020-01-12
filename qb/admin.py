1bf0b238e864ba7b294ffb74d14ee282252afcee
panu2306 <pranavbhendawade@gmail.com>
2020-01-12 14:59:07 +0530

    models created

--- qb/admin.py
+++ qb/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Question, Choice
 
 # Register your models here.
+#admin.site.register(Question)
+admin.site.register(Choice)
+ 
+@admin.register(Question)
+class VenueAdmin(admin.ModelAdmin):
+    list_display = ('question_text', 'lang')
+    #ordering = ('name',)
+    # search_fields = ('name', 'address')
--- qb/models.py
+++ qb/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+class Language(models.Model):
+    lang_name = models.CharField(max_length=64)
+    lang_code = models.CharField(max_length=2)
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=264)
+    lang = models.ForeignKey(Language, on_delete=models.PROTECT)
+    pub_date = models.DateTimeField('date_published')
+
+    def __str__(self):
+        return self.question_text 
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=64)
+    lang = models.ForeignKey(Language, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.choice_text
+
+    
+
+
