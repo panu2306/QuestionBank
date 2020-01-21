491b3fcdbbc21c14724761d5d69c2d3b9810f43c
panu2306 <pranavbhendawade@gmail.com>
2020-01-21 08:25:51 +0530

    Added field_name test for Question Model

--- qb/tests.py
+++ qb/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from .models import Question, Language, Choice
 
 # Create your tests here.
+class ModelQuestionTest(TestCase):
+    
+    
+    """
+        To check mentined field names are same as in the database.
+    """
+    def test_field_names(self):
+        field_names = [
+            field.name for field in Question._meta.get_fields()
+            ]
+        expected_field_names = ["choice", "id", "question_text", "pub_date", "lang"]
+        self.assertEqual(expected_field_names, field_names)
