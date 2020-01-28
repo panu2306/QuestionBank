34d5038712bc35941e1baa06cad078fd6476b6a8
panu2306 <pranavbhendawade@gmail.com>
2020-01-28 13:10:49 +0530

    Models structre changed.

--- qb/admin.py
+++ qb/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Question, Choice, Language, QuestionsInMultipleLanguage
+from .models import Question, Choice, Language
 
 # Register your models here.
 
@@ -10,25 +10,15 @@ class ChoiceInLine(admin.TabularInline):
 
 @admin.register(Question)
 class QuestionAdmin(admin.ModelAdmin):
-    inlines = [ChoiceInLine]
-    list_display = ('question_text', )
-    fields = ('lang', 'question_text', 'pub_date')
-    list_filter = ('lang', )
-    search_fields = ('question_text', 'lang')
-    filter_vertical = ('lang', )
-    date_hierarchy = 'pub_date'
-    """
-    fieldsets = ((None, {
-                    'fields': ('lang', )
-                }),
-                 ('Advanced Option', {
-                    'classes': ('collapse',),
-                    'fields': ('question_text', 'pub_date',),
-                }),
-            )
-    """
+    list_display = ('question_id', 'question_name', 'lang', 'pub_date')
+    list_display_links = ('question_name', )
+    list_editable = ('lang', )
+    inlines = [
+        ChoiceInLine,
+    ]
+    list_filter = ('question_id', 'lang',)
+    
 class LanguageAdmin(admin.ModelAdmin):
     list_display = ('lang_name', 'lang_code')
 
 admin.site.register(Language, LanguageAdmin)
-admin.site.register(QuestionsInMultipleLanguage)
--- qb/models.py
+++ qb/models.py
@@ -12,29 +12,18 @@ class Language(models.Model):
     
 
 class Question(models.Model):
-    question_text = models.CharField(max_length=264)
-    lang = models.ManyToManyField(Language)
+    question_id = models.IntegerField()
+    question_name = models.CharField(max_length=264)
+    lang = models.ForeignKey(Language, on_delete=models.PROTECT)
     pub_date = models.DateTimeField('date_published')
 
     def __str__(self):
-        return self.question_text 
+        return self.question_name
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.PROTECT)
     choice_text = models.CharField(max_length=64)
-    lang = models.ManyToManyField(Language)
+    lang = models.ForeignKey(Language, on_delete=models.PROTECT)
 
     def __str__(self):
         return self.choice_text
-
-class QuestionsInMultipleLanguage(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.PROTECT)
-    question_text = models.CharField(max_length=264)
-    question_language = models.ForeignKey(Language, on_delete=models.PROTECT)
-
-    def __str__(self):
-        return f"{self.question_text} - {self.question_language}"
-    
-
-
-
