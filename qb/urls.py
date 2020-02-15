4f63a911705dd0252c173c70e9630115f421a7f9
panu2306 <pranavbhendawade@gmail.com>
2020-02-15 13:19:36 +0530

    added index & form template for Question Model

--- qb/urls.py
+++ qb/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index, name='index'),
-    path('try_form', views.try_form, name='try_form'),
-    path('ack', views.acknowledge, name='ack-page')
+    #path('',views.index, name='index'),
+    path('', views.index, name='try_form'),
+    #path('ack', views.acknowledge, name='ack-page'), 
 ]
--- qb/views.py
+++ qb/views.py
@@ -4,24 +4,24 @@ from .models import Question, Choice
 from .forms import NameForm
 
 # Create your views here.
+# def index(request):
+#     questions = Question.objects.all()
+#     choices = Choice.objects.all()
+
+#     context = {
+#         'questions':questions,
+#         'choices':choices,
+#     }
+#     return render(request, 'qb/index.html', context=context)
+
 def index(request):
-    questions = Question.objects.all()
-    choices = Choice.objects.all()
-
-    context = {
-        'questions':questions,
-        'choices':choices,
-    }
-    return render(request, 'qb/index.html', context=context)
-
-def try_form(request):
     if request.method == 'POST':
         form = NameForm(request.POST)
         if form.is_valid():
-            return render(request, 'qb/thanks.html', context=None)
+            return render(request, 'qb/form_page.html', context=None)
     else:
         form = NameForm()
-    return render(request, 'qb/try_form.html', context={'form': form})
+    return render(request, 'qb/index.html', context={'form': form})
 
 def acknowledge(request):
-    return HttpResponse("Form Submitted Successfully!!!"    )
+    return HttpResponse("Form Submitted Successfully!!!")
