eb6176af9c22e1c5d955e7dc32081178d93e11ff
panu2306 <pranavbhendawade@gmail.com>
2020-02-19 15:45:20 +0530

    Modified admin directory structure & added custom view 'my_view'

--- qb/admin.py
+++ qb/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
+from django.urls import path 
+from django.shortcuts import render
+
 from .models import Question, Choice, Language
 
 # Register your models here.
@@ -11,13 +14,25 @@ class ChoiceInLine(admin.TabularInline):
 @admin.register(Question)
 class QuestionAdmin(admin.ModelAdmin):
     list_display = ('question_id', 'question_name', 'lang', 'pub_date')
-    list_display_links = ('question_name', )
+    list_display_links = ('question_id', )
     list_editable = ('lang', )
     inlines = [
         ChoiceInLine,
     ]
     list_filter = ('question_id', 'lang',)
-    
+    def get_urls(self):
+        urls = super().get_urls()
+        my_urls = [
+            path('my_view/', self.admin_site.admin_view(self.my_view, cacheable=True)),
+        ]
+        return my_urls + urls
+    def my_view(self, request):
+        return render(request, 'multilingual.html', context=None)
+    #change_form_template = 'admin/multilingual.html'
+
+
+
+
 class LanguageAdmin(admin.ModelAdmin):
     list_display = ('lang_name', 'lang_code')
 
