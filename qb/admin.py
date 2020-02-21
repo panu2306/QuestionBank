956babc5dc227a1814b913c1cf58f4df84367566
panu2306 <pranavbhendawade@gmail.com>
2020-02-21 13:06:20 +0530

    Added custom form for QuestionAdmin as well as form validation to its pub_date

--- qb/admin.py
+++ qb/admin.py
@@ -1,18 +1,38 @@
 from django.contrib import admin
 from django.urls import path 
 from django.shortcuts import render
+from django import forms
+from django.utils import timezone
 
 from .models import Question, Choice, Language
 
 # Register your models here.
 
+class QuestionAdminForm(forms.ModelForm):
+    class Meta:
+        model = Question
+        fields = '__all__'
+        widgets = {
+            'question_name': forms.Textarea(attrs={'cols': 50, 'rows': 10}),
+        }
+        localized_fields = ('question_name',)
+    
+    def clean(self):
+        cleaned_data = super().clean()
+        pub_date = cleaned_data.get('pub_date')
+        print(pub_date)
+        now = timezone.now()
+        if(pub_date > now):
+            raise forms.ValidationError("You are entering the future date.")
+
 class ChoiceInLine(admin.TabularInline):
     model = Choice
     extra = 2
-    max_num = 4
+    max_num = 4 
 
 @admin.register(Question)
 class QuestionAdmin(admin.ModelAdmin):
+    form = QuestionAdminForm
     list_display = ('question_id', 'question_name', 'lang', 'pub_date')
     list_display_links = ('question_id', )
     list_editable = ('lang', )
@@ -27,12 +47,8 @@ class QuestionAdmin(admin.ModelAdmin):
         ]
         return my_urls + urls
     def my_view(self, request):
-        return render(request, 'multilingual.html', context=None)
-    #change_form_template = 'admin/multilingual.html'
-
-
-
-
+        return render(request, 'admin/qb/multilingual.html', context=None)
+        
 class LanguageAdmin(admin.ModelAdmin):
     list_display = ('lang_name', 'lang_code')
 
