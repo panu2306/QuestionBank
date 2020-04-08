88872dd29c012faba2955a7683d082b14344b73e
panu2306 <pranavbhendawade@gmail.com>
2020-04-08 19:41:01 +0530

    UserProfileInfo class is added

--- qb/models.py
+++ qb/models.py
@@ -1,8 +1,17 @@
 from django.db import models
 from django.utils import timezone
 from django.core.exceptions import ValidationError
+from django.contrib.auth.models import User
 
 # Create your models here.
+class UserProfileInfo(models.Model):
+    user = models.OneToOneField(User, on_delete=models.PROTECT)
+
+    portfolio_site = models.URLField(blank=True)
+    profile_pic = models.ImageField(upload_to='profile_pics', blank=True)
+
+    def __str__(self):
+        return self.user.username
 
 class Language(models.Model):
     lang_name = models.CharField(max_length=64)
