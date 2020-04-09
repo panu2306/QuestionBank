8e6bf30e6ff5a6a415428e62653536c4d7e43ab9
panu2306 <pranavbhendawade@gmail.com>
2020-04-09 11:39:18 +0530

    improved navbar

--- qb_01/settings.py
+++ qb_01/settings.py
@@ -105,7 +105,6 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 PASSWORD_HASHERS = [
-    'django.contrib.auth.hashers.Argon2PasswordHasher',
     'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
     'django.contrib.auth.hashers.BCryptPasswordHasher',
     'django.contrib.auth.hashers.PBKDF2PasswordHasher',
